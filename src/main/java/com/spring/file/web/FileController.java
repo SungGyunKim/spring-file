274993bf28acaaffd816dda132253af4c402e162
KimSungGyun <egoksg@osstem.com>
2024-06-05 13:58:40 +0900

    stream api 추가

--- src/main/java/com/spring/file/web/FileController.java
+++ src/main/java/com/spring/file/web/FileController.java
@@ -13,8 +13,11 @@ import java.nio.file.Path;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
+import org.springframework.core.io.support.ResourceRegion;
 import org.springframework.http.ContentDisposition;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRange;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.MediaTypeFactory;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +25,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -93,6 +97,32 @@ public class FileController {
         .contentType(contentType)
         .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
         .body(resource);
+  }
+
+  @GetMapping("/{fileId}/stream")
+  public ResponseEntity<ResourceRegion> stream(@PathVariable String fileId,
+      @RequestHeader HttpHeaders httpHeaders)
+      throws Exception {
+    FileDto fileDto = fileService.findByFileId(fileId);
+    String filename = String.join(".", fileDto.getFileName(), fileDto.getFileExtension());
+    Path filePath = Path.of(fileDto.getFilePath(), fileDto.getFileId());
+    Resource resource = UrlResource.from(filePath.toUri());
+
+    long chunkSize = 1024 * 1024;
+    HttpRange httpRange = httpHeaders.getRange().stream()
+        .findFirst()
+        .orElse(HttpRange.createByteRange(0, resource.contentLength() - 1));
+    long start = httpRange.getRangeStart(resource.contentLength());
+    long end = httpRange.getRangeEnd(resource.contentLength());
+    long rangeLength = Long.min(chunkSize, end - start + 1);
+
+    MediaType contentType = MediaTypeFactory.getMediaType(filename)
+        .orElse(MediaType.APPLICATION_OCTET_STREAM);
+    ResourceRegion resourceRegion = new ResourceRegion(resource, start, rangeLength);
+
+    return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT)
+        .contentType(contentType)
+        .body(resourceRegion);
   }
 
 }
